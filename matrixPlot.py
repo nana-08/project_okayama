b07c3b281cb56489a1d62e19de8d1e0bdb475a11
Nana <loona.mac@gmail.com>
2023-08-04 15:07:01 +0900

    data plot

--- matrixPlot.py
+++ matrixPlot.py
@@ -4,9 +4,11 @@ import numpy as np
 x = np.arange(2,11)
 
 # ITERATIONS
+regIterThreads = [157,346,2571,20464,54117,76610,16168,614895,77896]
+regIterRpi = [126.5,265,1891,15284.2,43151.83,61298.28,13028.75,313370.44,62296.8]
 diagIterThreads = [36,61,85,109,133,159,182,207,228]
 diagIterRpi = []
-upTriIterThreads = [593,1000,1400,4719,5352581]
+upTriIterThreads = [593,1000,1400,4719,5352581,7229291]
 upTriIterRpi = []
 lowTriIterThreads = []
 lowTriIterRpi = []
@@ -14,9 +16,11 @@ symIterThreads = []
 symIterRpi = []
 
 # TIME
+regTimeThread = [0.164,0.495,5.333,52.231,162.658,271.058,67.461,2835.893,394.867]
+regTimeRpi = [3.351,11.797,124.232,1244.566,4541.231,6927.379,1715.195,43484.358,9846.849]
 diagTimeThreads = [0.00698,0.02083,0.04488,0.0734,0.10785,0.1518,0.20711,0.28476,0.3682]
 diagTimeRpi = []
-upTriTimeThreads = [0.08028,0.28124,0.64003,3.03326,10889.80274] 
+upTriTimeThreads = [0.08028,0.28124,0.64003,3.03326,10889.80274,63595.1072] 
 upTriTimeRpi = []
 lowTriTimeThreads = []
 lowTriTimeRpi = []
--- timePlot.py
+++ timePlot.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.arange(start=2,stop=9)
+x = np.arange(start=2,stop=11)
 
 
 ### PLOT
 plt.subplot(121)
 regIterThreads = [157,346,2571,20464,54117,76610,16168,614895,77896]
-regIterRpi = [126.5,265,1891,15284.2]
+regIterRpi = [126.5,265,1891,15284.2,43151.83,61298.28,13028.75,313370.44,62296.8]
 plt.plot(x, regIterThreads, "-ro", label="Multithread")
 plt.plot(x, regIterRpi, "-bo", label="Raspberry Pi")
 plt.xlabel("Number of agents")
@@ -19,20 +19,20 @@ annoThread = [str(int(y)) for y in regIterThreads]
 for xi, yi, text in zip(x, regIterThreads, annoThread):
     plt.annotate(text,
                 xy=(xi, yi), xycoords='data',
-                xytext=(10, 10), textcoords='offset points',
+                xytext=(-10, 15), textcoords='offset points',
                 bbox=dict(boxstyle="square,pad=0.3", fc="white", ec="red"))
     
 annoRpi = [str(int(y)) for y in regIterRpi]
 for xi, yi, text in zip(x, regIterRpi, annoRpi):
     plt.annotate(text,
                 xy=(xi, yi), xycoords='data',
-                xytext=(10, -10), textcoords='offset points',
+                xytext=(-10, -20), textcoords='offset points',
                 bbox=dict(boxstyle="square,pad=0.3", fc="white", ec="blue"))
 
 
 plt.subplot(122)
-regTimeThread = [0.16432,0.49525,5.33365,52.23167,162.65825,271.0581,67.46152,2835.89328,394.86779]
-regTimeRpi = [3.35060,11.79704,124.23232,1244.56652]
+regTimeThread = [0.164,0.495,5.333,52.231,162.658,271.058,67.461,2835.893,394.867]
+regTimeRpi = [3.351,11.797,124.232,1244.566,4541.231,6927.379,1715.195,43484.358,9846.849]
 plt.plot(x, regTimeThread, "-ro", label="Multithread")
 plt.plot(x, regTimeRpi, "-bo", label="Raspberry Pi")
 plt.xlabel("Number of agents")
@@ -44,14 +44,14 @@ annoThread = [str(round(y, 6))+" s" for y in regTimeThread]
 for xi, yi, text in zip(x, regTimeThread, annoThread):
     plt.annotate(text,
                 xy=(xi, yi), xycoords='data',
-                xytext=(10, -10), textcoords='offset points',
+                xytext=(-10, -20), textcoords='offset points',
                 bbox=dict(boxstyle="square,pad=0.3", fc="white", ec="red"))
     
 annoRpi = [str(round(y, 6))+" s" for y in regTimeRpi]
 for xi, yi, text in zip(x, regTimeRpi, annoRpi):
     plt.annotate(text,
                 xy=(xi, yi), xycoords='data',
-                xytext=(10, 10), textcoords='offset points',
+                xytext=(-10, 15), textcoords='offset points',
                 bbox=dict(boxstyle="square,pad=0.3", fc="white", ec="blue"))
 
 
